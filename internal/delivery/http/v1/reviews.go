7b5e2f3ecf164629b3654018935d05574f9fe94c
paw1a <paw1a@yandex.ru>
2022-01-12 22:13:31 +0300

    Add /reviews endpoints

---
+++ internal/delivery/http/v1/reviews.go
@@ -0,0 +1,106 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/paw1a/ecommerce-api/internal/domain"
+	"github.com/paw1a/ecommerce-api/internal/domain/dto"
+	"net/http"
+)
+
+func (h *Handler) initProductsRoutes(api *gin.RouterGroup) {
+	users := api.Group("/products")
+	{
+		users.GET("/", h.getAllProducts)
+		users.GET("/:id", h.getProductById)
+		users.POST("/", h.createProduct)
+		users.PUT("/:id", h.updateProduct)
+		users.DELETE("/:id", h.deleteProduct)
+	}
+}
+
+func (h *Handler) getAllProducts(context *gin.Context) {
+	products, err := h.services.Products.FindAll(context.Request.Context())
+	if err != nil {
+		newResponse(context, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	productsArray := make([]domain.Product, len(products))
+	if products != nil {
+		productsArray = products
+	}
+
+	context.JSON(http.StatusOK, dataResponse{Data: productsArray})
+}
+
+func (h *Handler) getProductById(context *gin.Context) {
+	id, err := parseIdFromPath(context, "id")
+	if err != nil {
+		newResponse(context, http.StatusBadRequest, err.Error())
+		return
+	}
+	product, err := h.services.Products.FindByID(context.Request.Context(), id)
+	if err != nil {
+		newResponse(context, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	context.JSON(http.StatusOK, dataResponse{Data: product})
+}
+
+func (h *Handler) createProduct(context *gin.Context) {
+	var productDTO dto.CreateProductDTO
+	err := context.BindJSON(&productDTO)
+	if err != nil {
+		newResponse(context, http.StatusBadRequest, "Invalid input body")
+		return
+	}
+	product, err := h.services.Products.Create(context.Request.Context(), productDTO)
+	if err != nil {
+		newResponse(context, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	context.JSON(http.StatusOK, dataResponse{Data: product})
+}
+
+func (h *Handler) updateProduct(context *gin.Context) {
+	var productDTO dto.UpdateProductDTO
+
+	err := context.BindJSON(&productDTO)
+	if err != nil {
+		newResponse(context, http.StatusBadRequest, "Invalid input body")
+		return
+	}
+
+	productID, err := parseIdFromPath(context, "id")
+	if err != nil {
+		newResponse(context, http.StatusBadRequest, err.Error())
+		return
+	}
+	productDTO.ID = productID
+
+	product, err := h.services.Products.Update(context.Request.Context(), productDTO)
+	if err != nil {
+		newResponse(context, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	context.JSON(http.StatusOK, dataResponse{Data: product})
+}
+
+func (h *Handler) deleteProduct(context *gin.Context) {
+	productID, err := parseIdFromPath(context, "id")
+	if err != nil {
+		newResponse(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err = h.services.Products.Delete(context, productID)
+	if err != nil {
+		newResponse(context, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	context.Status(http.StatusOK)
+}
---
+++ internal/service/reviews.go
@@ -0,0 +1,53 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/paw1a/ecommerce-api/internal/domain"
+	"github.com/paw1a/ecommerce-api/internal/domain/dto"
+	"github.com/paw1a/ecommerce-api/internal/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ProductsService struct {
+	repo repository.Products
+}
+
+func (p ProductsService) FindAll(ctx context.Context) ([]domain.Product, error) {
+	return p.repo.FindAll(ctx)
+}
+
+func (p ProductsService) FindByID(ctx context.Context, productID primitive.ObjectID) (domain.Product, error) {
+	return p.repo.FindByID(ctx, productID)
+}
+
+func (p ProductsService) Create(ctx context.Context, product dto.CreateProductDTO) (domain.Product, error) {
+	return p.repo.Create(ctx, domain.Product{
+		Name:        product.Name,
+		Description: product.Description,
+		Price:       product.Price,
+		Categories:  product.Categories,
+	})
+}
+
+func (p ProductsService) Update(ctx context.Context, productDTO dto.UpdateProductDTO) (domain.Product, error) {
+	fmt.Println(productDTO)
+	return p.repo.Update(ctx, dto.UpdateProductInput{
+		ID:          productDTO.ID,
+		Name:        productDTO.Name,
+		Description: productDTO.Description,
+		Price:       productDTO.Price,
+		TotalRating: productDTO.TotalRating,
+		Categories:  productDTO.Categories,
+	})
+}
+
+func (p ProductsService) Delete(ctx context.Context, productID primitive.ObjectID) error {
+	return p.repo.Delete(ctx, productID)
+}
+
+func NewProductsService(repo repository.Products) *ProductsService {
+	return &ProductsService{
+		repo: repo,
+	}
+}
