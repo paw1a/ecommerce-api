b94d1c15599d7be58fb12451bc3171c6d118402d
paw1a <paw1a@yandex.ru>
2022-04-19 19:18:39 +0300

    try cors

--- frontend/src/components/catalog/Catalog.jsx
+++ frontend/src/components/catalog/Catalog.jsx
@@ -8,9 +8,8 @@ import ProductCard from "./Card";
 
 const Catalog = () => {
     let [products, setProducts] = useState([]);
-
     useEffect(() => {
-        axios.get('http://localhost:8080/api/v1/products/')
+        axios.get('http://52.29.184.51:8080/api/v1/products/')
             .then((resp) => {
                 const products = resp.data.data;
                 setProducts(products);
