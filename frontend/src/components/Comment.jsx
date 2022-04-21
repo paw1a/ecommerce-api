cb09a607a544be0f0ad994ed57ac241f4bd465b9
paw1a <paw1a@yandex.ru>
2022-04-21 17:10:36 +0300

    add comments to product page

---
+++ frontend/src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import StarIcon from '@mui/icons-material/Star';
+
+import './Comment.css'
+
+export default function Comment() {
+    return (
+        <Card className='comment'>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe"/>
+                }
+                title="Shrimp and Chorizo Paella"
+                subheader="September 14, 2016"
+                action={
+                    <IconButton aria-label="settings" disabled>
+                        <StarIcon/>
+                        5
+                    </IconButton>
+                }
+                sx={{paddingLeft: 0}}
+            />
+            <CardContent sx={{paddingLeft: 0}}>
+                <Typography variant="body2" color="text.secondary">
+                    This impressive paella is a perfect party dish and a fun meal to cook
+                    together with your guests. Add 1 cup of frozen peas along with the mussels,
+                    if you like.
+                </Typography>
+            </CardContent>
+
+        </Card>
+    );
+}
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,17 +7,19 @@ import IconButton from "@mui/material/IconButton";
 import StarIcon from "@mui/icons-material/Star";
 import ModeCommentIcon from "@mui/icons-material/ModeComment";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Comment from "./Comment";
 
 const Product = () => {
     return (
         <div className='container'>
             <Grid container columnSpacing={2}>
-                <Grid item xs={12} md={4}>
+                <Grid item xs={12} md={5}>
                     <img src={image} alt="image" className='image'/>
                 </Grid>
-                <Grid item xs={12} md={8}>
+                <Grid item xs={12} md={7}>
                     <div className='content'>
-                        <h1>Name of the product</h1>
+                        <h1 style={{marginTop: 0}}>Name of the product</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto culpa debitis doloremque expedita facere illo incidunt, repellendus sint unde voluptate. Amet, aperiam autem corporis debitis eligendi facere harum laborum libero magnam possimus. A beatae, debitis dignissimos esse iure laborum minus nihil nisi nulla obcaecati odio odit perferendis quis sunt, tenetur?</p>
                         <div>
                             <IconButton disableRipple='true' style={{paddingLeft: 0}}>
@@ -32,10 +34,24 @@ const Product = () => {
                         </div>
 
                         <div className='price'>12000 <span style={{color: "gray"}}>$</span></div>
-                        <Button variant='outlined' color='inherit'>Add to cart</Button>
-                        <IconButton className='bookmark' disableRipple='true'>
+                        <Button variant="outlined" color='inherit' startIcon={
+                            <ShoppingCartIcon fontSize='large' />
+                        }>
+                            Add to cart
+                        </Button>
+                        <IconButton className='bookmark' disableRipple='true' size='large'>
                             <BookmarkIcon className='product-button'/>
                         </IconButton>
+
+                        <div className='comments-container'>
+                            <h2>Comments</h2>
+                            <Comment/>
+                            <Comment/>
+                            <Comment/>
+                            <Comment/>
+                            <Comment/>
+                        </div>
+
                     </div>
                 </Grid>
             </Grid>
